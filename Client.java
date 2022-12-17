ec8ca65568354d28b34a81db639e9b77fe3869aa
unknown <voduynhan.123@gmail.com>
2022-12-17 23:14:39 +0700

    fix: msg structure sent from client to server

--- Client.java
+++ Client.java
@@ -1,72 +1,73 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.Scanner;
-
 
 public class Client {
     static int BYTE_LENGTH = 100;
-    public static void main(String[] args) throws  IOException {
-    Socket s = new Socket("localhost", 3300);
-    
-    if (s.isConnected()) System.out.println("Connected");
-    
-    /* READ THE PATH TO FOLDER, SENT FROM SEVER */
-    InputStream is = s.getInputStream();
-    BufferedReader br = new BufferedReader(new InputStreamReader(is));
-    String path = br.readLine();
-    System.out.println(path);
 
-    File file = new File(path);
-    if (file.isDirectory()) System.out.println("Directory");
-    else System.out.println("File");
+    public static void main(String[] args) throws IOException {
+        Socket s = new Socket("localhost", 3300);
 
-    OutputStream os = s.getOutputStream();
-    byte[] msg = new byte[BYTE_LENGTH];
+        if (s.isConnected())
+            System.out.println("Connected");
 
+        /* READ THE PATH TO FOLDER, SENT FROM SEVER */
+        InputStream is = s.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String path = br.readLine();
 
-    do {
-        String[] directories = file.list();
+        File file = new File(path);
+        // if (file.isDirectory())
+        //     System.out.println("Directory");
+        // else
+        //     System.out.println("File");
 
-        /*SEND SUBFOLDER NAME TO SERVER */
-        for (int i = 0 ; i < directories.length; i++)
-        {
-            String pathName = path + "\\" + directories[i];
-            if (new File(pathName).isDirectory()) os.write((directories[i] + "|subfoler").getBytes());
-            else
-            {
-                int sizeOfFile = 0;
-                // os.write((directories[i] + "|file").getBytes());
-                FileInputStream fis = new FileInputStream(pathName);
-                int length = fis.read(msg);
-                sizeOfFile += length;
-                if (length < BYTE_LENGTH) os.write((directories[i] + Integer.toString(sizeOfFile)).getBytes());
-                else
-                while (length >= BYTE_LENGTH){
-                    sizeOfFile += length;
-                    length = fis.read(msg);
+        OutputStream os = s.getOutputStream();
+        byte[] msg = new byte[BYTE_LENGTH];
 
+        do {
+            String[] directories = file.list();
+
+            /* SEND SUBFOLDER NAME TO SERVER */
+            for (int i = 0; i < directories.length; i++) {
+                String pathName = path + "\\" + directories[i];
+                if (new File(pathName).isDirectory()) // Check if 1 path name is directory or file
+                    os.write((directories[i] + "|subfolder|").getBytes()); // Send name|subfolder
+                else {
+                    int sizeOfFile = 0;
+                    // os.write((directories[i] + "|file").getBytes());
+                    /*READ THE SIZE OF THE FILE */
+                    /* IF SIZE CHANGE THEN FILE IS MODIFIED */
+                    FileInputStream fis = new FileInputStream(pathName);
+                    int length = fis.read(msg);
+                    // sizeOfFile += length;
+                    // if (length < BYTE_LENGTH)
+                    //     os.write((directories[i] + Integer.toString(sizeOfFile)).getBytes());
+                    // else
+                    //     while (length >= BYTE_LENGTH) {
+                    //         sizeOfFile += length;
+                    //         length = fis.read(msg);
+                    //     }
+                    //     sizeOfFile += length;
+                    // os.write((directories[i] + "|" + Integer.toString(sizeOfFile)).getBytes());
+                    while (length != -1){
+                        sizeOfFile += length;
+                        length = fis.read(msg);
+                    }
+                    os.write((directories[i] + "|" + Integer.toString(sizeOfFile) + "|").getBytes());
+                    fis.close();
                 }
-                os.write((directories[i] + Integer.toString(sizeOfFile)).getBytes());
-                fis.close();
             }
-        }
 
-        os.write("Done".getBytes());
+            os.write("Done".getBytes());
+
+        } while (true);
 
     }
-    while (true);
-    
-
-
-}
-
-    
 
 }
--- Server.java
+++ Server.java
@@ -7,12 +7,15 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
+/*LISTEN THREAD FOR RECEIVING INFORMATION FROM CLIENT */
 class ListenThread extends Thread {
     static int BYTE_LENGTH = 100;
+    static Boolean isInitial = true;
     Socket s;
-    HashMap<String,Integer>folders; // int is used to check if the folder checked or not: 0: Not checked, 1:
-                                         // Checked, 2: Created
+    HashMap<String, Integer> folders; // int is used to check if the folder checked or not: 0: Not checked, 1:
+                                      // Checked, 2: Created
     HashMap<String, Integer> files = null;
 
     ListenThread(Socket s) {
@@ -20,66 +23,77 @@ class ListenThread extends Thread {
         this.s = s;
     }
 
-    public void run()
-    {
+    public void run() {
         try {
-            if (this.folders == null) this.folders = new HashMap<String, Integer>();
-            if (this.files == null) this.files = new HashMap<String, Integer>();
+            if (this.folders == null)
+                this.folders = new HashMap<String, Integer>();
+            if (this.files == null)
+                this.files = new HashMap<String, Integer>();
 
             InputStream is = this.s.getInputStream();
             byte[] msg = new byte[BYTE_LENGTH];
             String line = "";
-            do{
+
+            do {
                 is.read(msg);
                 line = new String(msg);
+                System.out.println(line);
+                String[] parts = line.split(Pattern.quote("|"));
+
                 
-                String[] parts = line.split("|") ;
-                if (parts[1] == "folder")
-                {
-                    /*FOLDER PROCESS */
-                    if (this.folders.size() == 0) this.folders.put(parts[0], 2);
-                    else
-                    {
-                        if (this.folders.get(parts[0]) == null) 
-                        {
+                if (isInitial == false) {
+                    if (parts[1] == "subfolder") {
+                        /* FOLDER PROCESS */
+
+                        if (this.folders.get(parts[0]) == null) {
                             System.out.println("a subfolder created");
                             this.folders.put(parts[0], 2);
                         }
-                    
+
                         this.folders.remove(parts[0]);
-                        this.folders.put(parts[0],1);
-                    }
-                }
+                        this.folders.put(parts[0], 1);
 
-                else
-                {
-                    /* FILE PROCESS */
-                    int sizeOfFile = Integer.parseInt(parts[1]);
-                    if (this.files.get(parts[0])== null)
-                    {
-                        System.out.println("a file created");
-                        this.files.put(parts[0], sizeOfFile);
                     }
 
-                    
-                    if (this.files.get(parts[0]) != sizeOfFile) 
-                    {
-                        System.out.println("a file modified");
-                        this.files.remove(parts[0]);
-                        this.files.put(parts[0], sizeOfFile);
-                    }
-                }
-                
-                if (line == "Done") {
-                    for (String folder: this.folders.keySet())
-                        if (this.folders.get(folder)== 0) 
-                        {
-                            System.out.println("A folder deleted");
-                            this.folders.remove(folder);
+                    else if (parts[1] != null) {
+                        /* FILE PROCESS */
+                        int sizeOfFile = Integer.parseInt(parts[1]);
+                        if (this.files.get(parts[0]) == null) {
+                            System.out.println("a file created");
+                            this.files.put(parts[0], sizeOfFile);
                         }
+
+                        if (this.files.get(parts[0]) != sizeOfFile) {
+                            System.out.println("a file modified");
+                            this.files.remove(parts[0]);
+                            this.files.put(parts[0], sizeOfFile);
+                        }
+                    }
+
+                    if (line == "Done") {
+
+                        for (String folder : this.folders.keySet())
+                            if (this.folders.get(folder) == 0) {
+                                System.out.println("A folder deleted");
+                                this.folders.remove(folder);
+                            }
+                    }
                 }
-            }
-            while (true);
+
+                else {
+                    /* FIRST LET SERVER KNOW WHAT FOLDER STRUCTURE IS LIKE IN CLIENT */
+                    if (parts[1] == "subfolder") {
+                        this.folders.put(parts[0], 1);
+                    } else if (parts[1] != null) {
+                        int size = Integer.parseInt(parts[1]);
+                        this.files.put(parts[0], size);
+                    }
+
+                    if (line == "Done") {
+                        isInitial = false;
+                    }
+                }
+            } while (true);
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -89,6 +103,7 @@ class ListenThread extends Thread {
 
 }
 
+/* SERVER CLASS */
 public class Server {
     static int BYTE_LENGTH = 100;
 
